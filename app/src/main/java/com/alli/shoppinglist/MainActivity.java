54b308c6a566de4ab248a63b635c4f9176e18d4c
Alli Abdulateef <alli5723@gmail.com>
2018-04-22 08:30:30 +0300

    Added recycler view for shopping list

--- app/src/main/java/com/alli/shoppinglist/MainActivity.java
+++ app/src/main/java/com/alli/shoppinglist/MainActivity.java
@@ -1,7 +1,10 @@
 package com.alli.shoppinglist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_add) {
+            // TODO: Add a new item to list here
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/alli/shoppinglist/models/ShoppingItem.java
@@ -0,0 +1,10 @@
+package com.alli.shoppinglist.models;
+
+/**
+ * Created by omo_lanke on 22/04/2018.
+ */
+
+public class ShoppingItem {
+    String item;
+
+}
